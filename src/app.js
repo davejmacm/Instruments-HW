7865097eafbf1ce62662fe769a66f565c1628a4e
davejmacm <davejmacm@gmail.com>
2019-02-05 18:23:36 +0000

    added pub_sub helper and started on app

--- src/app.js
+++ src/app.js
@@ -1,3 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript Loaded');
+  const selectInstrument = document.querySelector('#instrument-families');
+  const instrumentDropdown = new SelectView(selectInstrument);
+  instrumentDropdown.bindEvents();
 });
