8ac638fa23cc47db7295e395f40ef9b55ae38105
davejmacm <davejmacm@gmail.com>
2019-02-05 20:41:15 +0000

    select view added

--- src/app.js
+++ src/app.js
@@ -1,3 +1,6 @@
+const InstrumentFamilies = require('.models/instrument_families.js');
+const SelectView = require('./views/select_view.js');
+
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript Loaded');
   const selectInstrument = document.querySelector('#instrument-families');
---
+++ src/views/select_view.js
@@ -0,0 +1,28 @@
+const PubSub = require('../helpers/pub_sub.js');
+
+const SelectView = function(element){
+  this.element = element;
+};
+
+SelectView.prototype.bindEvents = function(){
+  PubSub.subscribe('Instrument-families:all-instruments', (evt) => {
+    const allInstruments = evt.detail;
+    this.populate(allInstruments);
+  });
+
+  this.element.addEventListener('change', (evt) => {
+    const selectedIndex = evt.target.value;
+    PubSub.publish('SelectView:change', selectedIndex);
+  });
+};
+
+SelectView.prototype.populate = function(instrumentFamily){
+  instrumentFamily.forEach((instrument, index) => {
+    const option = document.createElement('instrument');
+    option.textContent = instrument.name;//this will only display name for just now - fix for everything once confirmed working
+    option.value = index;
+    this.element.appendChild(option);
+  })
+}
+
+module.exports = SelectView;
